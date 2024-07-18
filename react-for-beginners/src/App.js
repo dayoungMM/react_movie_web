f39f8488d7cee27f2daa0e47863c4a4bc1aa7512
dayoungMM <dalbeetm@gmail.com>
2024-07-18 20:01:10 +0900

    useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -2,27 +2,28 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
+  const [movies, setMovies] = useState([]);
+  // const fetch = require('node-fetch');
+
+  const url = 'https://api.themoviedb.org/3/authentication';
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzFiMTFhZjY4YWRmODZmNzRkNjk2N2I2YTIyYzNlYSIsIm5iZiI6MTcyMDQxNTU5MS4zMDgxNDEsInN1YiI6IjY2OGI3MjFmZjNkODRjZjFlYjk4YzA3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uL9SjrmW7h2iMymPK-BpgchfQlc3IcsPKvTLs5YCAak'
+    }
+  };
+
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers").then( (response) =>
-      response.json()
-    ).then((data) => {
-      // console.log(data);
-      setLoading(false);
-      setCoins(data);
-    });
-  }, []);
+    fetch(url, options)
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.error('error:' + err));
+  }, [])
   return (
     <div>
-      <h1>The Coins!</h1>
-      {loading ? <strong>Loading....</strong> : null}
-      <select>
-        {coins.map((coin) =>
-           <option>
-            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
-            </option>
-        )}
-      </select>
+      {loading ? <h1>Loading...</h1> : null}
+      {movies.map((movie) => <li>{movie.name}</li>)}
     </div>
   )
 }
