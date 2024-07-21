ec96cb78cd53f2e8ce1c6f868e069dfe20891eae
dayoungMM <dalbeetm@gmail.com>
2024-07-21 16:52:35 +0900

    css

--- react-for-beginners/src/components/Movie.js
+++ react-for-beginners/src/components/Movie.js
@@ -1,15 +1,17 @@
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
+import styles from './Movie.module.css';
 
-function Movie({id, medium_cover_image, title, summary, genres}){
+function Movie({id, year, medium_cover_image, title, summary, genres}){
     return (
-        <div >
-            <img src={medium_cover_image} alt={title} />
-            <h2>
+        <div className={styles.movie}>
+            <img src={medium_cover_image} alt={title} className={styles.movie__img} />
+            <h2 className={styles.movie__title}>
               <Link to={`/movie/${id}`}>{title}</Link>
             </h2>
-            <p>{summary}</p>
-            <ul>
+            <h3 className={styles.movie__year}>{year}</h3>
+            <p>{summary.length > 300 ? `${summary.slice(0,300)}...` : summary}</p>
+            <ul className={styles.movie__genres}>
 
               {genres.map((genre, index) => (
                 <li key={index}>{genre}</li>
@@ -22,6 +24,7 @@ function Movie({id, medium_cover_image, title, summary, genres}){
 
 Movie.propTypes = {
     id: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
     medium_cover_image: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
--- react-for-beginners/src/routes/Home.js
+++ react-for-beginners/src/routes/Home.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import Movie from '../components/Movie';
-
+import styles from './Home.module.css';
 function Home() {
     const [loading, setLoading] = useState(true);
 
@@ -20,13 +20,14 @@ function Home() {
         getMovies();
     }, [])
     return (
-        <div>
+        <div className={styles.container}>
             {loading ? <h1>Loading...</h1> : null}
-            <div>
+            <div className={styles.movies}>
                 {movies.map((movie, index) => (
                     <Movie
                         key={index}
                         id={movie.id}
+                        year={movie.year}
                         medium_cover_image={movie.medium_cover_image}
                         title={movie.title}
                         summary={movie.summary}
